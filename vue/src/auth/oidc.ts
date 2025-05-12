9de2b422cca648328c8928544ffcacef02cbfb27
ronny-standtke <ronny.standtke@conplement.de>
2025-05-12 11:52:02 +0200

    set keycloak auth url

--- vue/src/auth/oidc.ts
+++ vue/src/auth/oidc.ts
@@ -1,7 +1,7 @@
 import { InMemoryWebStorage, UserManager, WebStorageStateStore } from "oidc-client-ts"
 
 const oidcConfig = {
-	authority: "https://keycloak.omnect.conplement.cloud/realms/cp-dev",
+	authority: import.meta.env.VITE_KEYCLOAK_AUTH,
 	client_id: "omnect-ui",
 	redirect_uri: `https://${window.location.hostname}:${window.location.port}/auth-callback`,
 	response_type: "code",
