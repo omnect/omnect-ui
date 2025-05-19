8d78e1e12ef71123c3ba70777e2b3d50eeba21df
ronny-standtke <ronny.standtke@conplement.de>
2025-05-19 10:27:05 +0200

    add docu link

--- vue/src/components/BaseSideBar.vue
+++ vue/src/components/BaseSideBar.vue
@@ -36,10 +36,14 @@ onGotResponse(async (res) => {
 				color="white" active-class="text-white bg-white/10" :to="route.path">
 				<v-list-item-title :style="{ fontWeight: 700 }"> {{ (route.meta!.text as string) }}</v-list-item-title>
 			</v-list-item>
+			<v-list-item active-class="text-white bg-white/10" target="_blank" rel="noopener noreferrer"
+				href="https://documentation.omnect.conplement.cloud/" class="text-white hover:bg-white/10">
+				<v-list-item-title :style="{ fontWeight: 700 }">Documentation</v-list-item-title>
+			</v-list-item>
 		</v-list>
 		<template v-slot:append>
 			<div class="flex flex-col items-center mb-4">
-				<div class="text-center w-40 text-sm lowercase">(v.{{ version }})</div>
+				<div class="text-center w-40 text-sm lowercase"> (v.{{ version }})</div>
 				<a :class="`text-white hover:underline-white`" class="decoration-underline"
 					href="https://www.conplement.de/en/impressum-legal-notice" target="_blank">Imprint</a>
 			</div>
