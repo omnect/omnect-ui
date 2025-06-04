d073e1cf647c9c52b45978c2b1bab6e6530a8461
ronny-standtke <ronny.standtke@conplement.de>
2025-06-04 11:17:30 +0200

    feat: manuell reload if reset or reboot takes to long

--- vue/src/composables/useWaitReconnect.ts
+++ vue/src/composables/useWaitReconnect.ts
@@ -3,18 +3,25 @@ import { ref } from "vue"
 import { useEventHook } from "./useEventHook"
 
 const connectedEvent = useEventHook()
+const timeoutEvent = useEventHook()
 
 export function useWaitReconnect() {
 	const wasDown = ref(false)
 	const reconnectInterval = ref()
+	const reconnectTimeout = ref()
 
 	const startWaitReconnect = async () => {
 		reconnectInterval.value = setInterval(checkReconnect, 5_000)
+		reconnectTimeout.value = setTimeout(() => {
+			timeoutEvent.trigger()
+			clearTimeout(reconnectTimeout.value)
+		}, 300_000)
 	}
 
 	const stopWaitReconnect = () => {
 		wasDown.value = false
 		clearInterval(reconnectInterval.value)
+		clearTimeout(reconnectTimeout.value)
 		connectedEvent.trigger()
 	}
 
@@ -36,8 +43,10 @@ export function useWaitReconnect() {
 			if (err.isAxiosError && err.code === "ECONNABORTED") {
 				wasDown.value = true
 			}
+			//ERR_CERT_AUTHORITY_INVALID
+			console.error("Error checking reconnect:", error)
 		}
 	}
 
-	return { startWaitReconnect, stopWaitReconnect, onConnected: connectedEvent.on }
+	return { startWaitReconnect, stopWaitReconnect, onConnected: connectedEvent.on, onTimeout: timeoutEvent.on }
 }
