19b7579d3fcc1ef75dc4cbf1f32fe4c02284f549
Jan Zachmann <50990105+JanZachmann@users.noreply.github.com>
2025-10-31 18:14:31 +0100

    refactor: introduce NetworkConfigService pattern

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod api;
 pub mod auth;
+pub mod certificate;
 pub mod common;
 pub mod http_client;
 pub mod keycloak_client;
 pub mod middleware;
+pub mod network;
 pub mod omnect_device_service_client;
